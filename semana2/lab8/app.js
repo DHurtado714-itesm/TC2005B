56de40dd4b5357f21b7aff2c786608a1ad7f3ab6
DHurtado714-itesm <a01707774@itesm.mx>
2023-02-23 09:52:15 -0600

    setTimeout function implemented

--- semana2/lab8/app.js
+++ semana2/lab8/app.js
@@ -1,5 +1,5 @@
 console.log("Hola mundo");
-console.info("Este script se ejecuta desde la computadora del usuario")
+console.info("Este script se ejecuta desde la computadora del usuario");
 console.warn(
   "Docuemnt, alert, confirm, prompt, no existen ene el entorno de ejecución de Node.js"
 );
@@ -10,3 +10,13 @@ console.error("Error: no se pudo ejecutar el programa");
 const filesystem = require("fs");
 filesystem.writeFileSync("hola.txt", "Hola dede Node.js");
 console.log(filesystem.readFileSync("hola.txt").toString());
+
+const arreglo = [
+  1, 2, 3, 4, 5, 10, 50, 100, 200, 500, 1000, 2, 3, 10000, 50000,
+];
+
+for (let item of arreglo) {
+  setTimeout(() => {
+    console.log(item);
+  }, item);
+}
